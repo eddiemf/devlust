6e36382c8176222006c17bef2035d58f3afb2b53
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2019-10-14 19:45:29 +0200

    Fix container padding

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   max-width: 800px;
+  padding-left: 15px;
+  padding-right: 15px;
   margin-left: auto;
   margin-right: auto;
 `;
