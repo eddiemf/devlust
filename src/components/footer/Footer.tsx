3751bc03b5436491db68432a7b582bfc3a7c84d9
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2019-10-14 19:46:01 +0200

    Make gatsby footer fancier

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Container from '../container';
+import { brandColor } from '../../theme/colors';
 
 const Footer = () => {
   return (
     <StyledFooter>
       <Container>
-        © {new Date().getFullYear()} - Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <p>
+          © {new Date().getFullYear()} - Built with{' '}
+          <GatsbyLink href="https://www.gatsbyjs.org" target="_blank">
+            Gatsby ️❤️
+          </GatsbyLink>
+        </p>
       </Container>
     </StyledFooter>
   );
@@ -24,6 +28,14 @@ const StyledFooter = styled.footer`
   align-items: center;
   height: ${footerHeight}px;
   margin-top: ${footerMarginTop}px;
+  box-shadow: 0px -2px 4px 2px rgba(46, 41, 51, 0.06),
+    0px -2px 4px rgba(71, 63, 79, 0.08);
+`;
+
+const GatsbyLink = styled.a`
+  &:hover {
+    color: ${brandColor};
+  }
 `;
 
 export default Footer;
